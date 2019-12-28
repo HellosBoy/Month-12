34d5a0f7504527bee57c464a28a4a4a1790e2065
Helloboymy <2573291524@qq.@qq.com>
2019-12-28 08:37:05 +0800

    v

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <header>
+            <input type="text" v-model="url">
+        </header>
+        <main>
+            <div class="left">
+                <p :class="{active: curIndex === index}" @click="changeTab(index)" v-for="(item, index) in list" :key="index">{{item.name}}</p>
+            </div>
+            <RightView class="right" @changeUrl="changeUrl" :info="right" />
+        </main>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import RightView from './components/rightView'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        RightView
+    },
+    data(){
+        return {
+            list: [],
+            // 表示当前选中
+            curIndex: 0,
+            right: {},
+            url: ''
+        }
+    },
+    methods: {
+        changeTab(index){
+            this.curIndex = index;
+            this.right = this.list[this.curIndex]
+        },
+        changeUrl(url){
+            this.url = url
+        }
+    },
+    created(){
+        axios.get('/list').then(r => {
+            this.list = r.data.values
+        })
+    }
 }
 </script>
 
 <style lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+#app, html, body {
+    width: 100%;
+    height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+}
+header {
+    width: 100%;
+    height: 60px;
+    background: #cccccc;
+    input {
+        width: 100%;
+        height: 100%;
+    }
+}
+main {
+    flex: 1;
+    overflow: hidden;
+    display: flex;
+}
+.left {
+    width: 100px;
+    background: #66ccff;
+    overflow-y: auto;
+    p {
+        line-height: 45px;
+        border-bottom: 1px solid #cccccc;
+        &.active {
+            background: tomato;
+            color: #ffffff;
+        }
+    }
+} 
+.right {
+    flex: 1;
+    overflow: hidden;
 }
 </style>
---
+++ src/components/rightView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img @click="$emit('changeUrl', info.image)" :src="info.image" alt="">
+    </div>
+</template> 
+
+<script>
+export default {
+    props: ['info']                                                              
+}
+</script>11111
+
+<style scoped>
+img {
+    width: 100%;
+}
+
+</style>
